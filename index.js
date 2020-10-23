298de665906a518b0cb40c21a15fb49187dcd3c3
arjun.maini <arjun.maini@scalenut.com>
2020-10-23 11:40:15 +0530

    added twitter

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
-const twit = require('twit');
+const Twit = require('twit');
 const notifier = require('node-notifier');
 const open = require('open');
 const franc = require('franc');
+const ejs = require('ejs');
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const LoginWithTwitter = require('login-with-twitter');
+const app = express()
+const tw = new LoginWithTwitter({
+    consumerKey: 'kbZS75go130eVtCKtjRMPpv2b',
+    consumerSecret: 'ItW7MICvwmDIroGrvvtcInhhw7Z0eDSjGJ0QZQ7OyF0hcHqLto',
+    callbackUrl: 'http://localhost:3000/sign'
+  })
 
-const apiKey = 'kbZS75go130eVtCKtjRMPpv2b'
-const apiSecretKey = 'ItW7MICvwmDIroGrvvtcInhhw7Z0eDSjGJ0QZQ7OyF0hcHqLto'
-const accessToken = '1318427402544975877-C2iZ1oz7RlWGq0XGkRon9DoqrhfgL6'
-const accessTokenSecret = 'eINKmOI9lARX81yQdXoitXX4pPigGUlm67jb892sDyfra'
-
-var T= new twit({
-
-    consumer_key : apiKey,
-    consumer_secret : apiSecretKey,
-    access_token : accessToken,
-    access_token_secret : accessTokenSecret
-
-});
+  app.use(express.urlencoded())
+;
+app.set('view engine', 'ejs')
+app.use(session({
+      secret:'argsbrhym',
+      cookie:{}
+  }))
 
 app.get('/', function(req, res){
-    res.send('Welcome to twitter app!');
+    res.render('index');
 });
+
+app.get('/login', function(req, res){
+    tw.login((err, tokenSecret, url) => {
+        if (err) {
+        }
+        
+        // Save the OAuth token secret for use in your /twitter/callback route
+        req.session.tokenSecret = tokenSecret
+
+        // Redirect to the /twitter/callback route, with the OAuth responses as query params
+        res.redirect(url)
+      })
+});
+
+app.get('/sign', (req, res) => {
+    var params = {
+        oauth_token : req.query.oauth_token,
+        oauth_verifier : req.query.oauth_verifier
+    } 
+
+    tw.callback(params, req.session.tokenSecret, (err, user) => {
+          req.session.user = user
+          res.redirect('/dash');
+    })
+});
+
+app.get('/dash', function(req, res){
+    if(req.session.user){
+        res.render('dash', {user : req.session.user})
+    }else{
+        res.redirect('/')
+    }
+});
+
+app.post('/postTweet', (req, res) => {
+    
+    var T = new Twit({
+        consumer_key:         'kbZS75go130eVtCKtjRMPpv2b',
+        consumer_secret:      'ItW7MICvwmDIroGrvvtcInhhw7Z0eDSjGJ0QZQ7OyF0hcHqLto',
+        access_token:         req.session.user.userToken,
+        access_token_secret:  req.session.user.userTokenSecret,
+    })
+
+    T.post('statuses/update', { status: req.body.tweet_content }, function(err, data, response) {
+        res.redirect('dash')
+      })
+
+      
+} )
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server started on port 3000'));
